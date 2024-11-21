fa9cc936107fc3701e3b95d2d775b3675366a7d0
talineshawwaa <talineshawwa15@gmail.com>
2024-11-21 17:19:01 +0100

    added abstract classes, containers, constructors, deconstructors, rule of 3/5, encapsulation

--- manager.h
+++ manager.h
@@ -5,27 +5,43 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <memory>
 
 using namespace std;
 
-// Base class for managing common operations
-class Manager {
+// Abstract base class for managing common operations
+class BaseManager{
 public:
-    void encrypt(const std::string& data);    // Encrypts data
-    bool validate(const std::string& password);  // Validates password strength
+    virtual void encrypt(const string& data) const =0; //Pure virtual function that encrypts data
+    virtual bool validate(const string& password) const =0; //Pure virtual function that validates password length
+    virtual ~BaseManager() = default; // Virtual deconstructor
 };
 
 // Derived class for managing passwords
-class PasswordManager : public Manager {
+class PasswordManager : public BaseManager {
 private:
-    map<string, pair<string, string>> credentials;  // Stores the credentials (service, username, password)
+    vector<pair<string, string>> credentials; // Container that stores the credentials (service, username, password)
     string username;
     string mainPassword;
 
     void saveCredentialsToFile();       // Save individual account credentials to a user-specific file
+    static const string encryptionKey;
 
 public:
+    // Constructors and rule of 3/5 application 
     PasswordManager();                  // Constructor for user login or registration
+    PasswordManager(const PasswordManager& other); //Copy constructor
+    PasswordManager(PasswordManager&& other) noexcept; //Move Constructor
+    PasswordManager& operator=(const PasswordManager& other); // Copy assignment
+    PasswordManager& operator=(PasswordManager&& other) noexcept; // Move assignment
+    ~PasswordManager(); // Destructor
+
+    // Encapsulation: Getters and Setters
+    void setUsername(const string& name) { username = name; }
+    const std::string& getUsername() const { return username; }
+    void setMainPassword(const std::string& password) { mainPassword = password; }
+    const std::string& getMainPassword() const { return mainPassword; }
+
     void addNewPassword(string serviceName, string serviceUsername, string password); // Add a new password for a service
     void showAllPasswords();            // Show all stored passwords
     void deletePassword(string serviceName);  // Delete a password by service name
